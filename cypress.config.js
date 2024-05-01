1a3a49fa2c36b38bdb8e196c5fc2527c71f329a2
degemar.silva <degemar.silva@sensedata.com.br>
2024-05-01 17:50:46 -0300

    config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
